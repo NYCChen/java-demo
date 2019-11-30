2703f22d32a18341176f3b81d19c57199c337eec
NYCChen <2654065626@qq.com>
2019-11-30 19:23:48 +0800

    第4题

--- src/leetcode/a01_a10/A04FindMedianSortedArrays.java
+++ src/leetcode/a01_a10/A04FindMedianSortedArrays.java
@@ -128,10 +128,74 @@ public class A04FindMedianSortedArrays {
             cut1 = (cutR - cutL) / 2 + cutL;
             cut2 = len / 2 - cut1;
             double L1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            double L2 = (cut2 == 0) ? Integer.MIN_VALUE : nums1[cut2 - 1];
-            double R1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1];
-            double R2 = (cut2 == 0) ? Integer.MIN_VALUE : nums1[cut2];
+            double L2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            double R1 = (cut1 == 0) ? Integer.MAX_VALUE : nums1[cut1];
+            double R2 = (cut2 == 0) ? Integer.MAX_VALUE : nums2[cut2];
+            if (L1 > R2) {
+                cutR = cut1 - 1;
+            } else if (L2 > R1) {
+                cutL = cut1 + 1;
+            } else {
+                if (len % 2 == 0) {
+                    L1 = L1 > L2 ? L1 : L2;
+                    R1 = R1 < R2 ? R1 : R2;
+                    return (L1 + R1) / 2;
+                } else {
+                    R1 = R1 < R2 ? R1 : R2;
+                    return R1; // 总长度为奇数时，中间值总是 R1与R2中的最小值。
+                }
+            }
         }
-        return 0;
+        return -1;
     }
 }
+
+/*
+public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+int MIN_VALUE = 0x80000000;
+
+        int MAX_VALUE = 0x7fffffff;
+
+        int N1 = nums1.length;
+        int N2 = nums2.length;
+        if (N1 > N2) {// 确保N1是短的部分。
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        if (N1 == 0)
+            return ((double) nums2[(N2 - 1) / 2] + (double) nums2[N2 / 2]) / 2;
+        int size = N1 + N2;
+        int cutL = 0, cutR = N1;
+        int cut1 = N1 / 2;
+        int cut2 = size / 2 - cut1;
+
+        while (cut1 <= N1) {
+            cut1 = (cutR - cutL) / 2 + cutL;
+            cut2 = size / 2 - cut1;
+
+            double L1 = (cut1 == 0) ? MIN_VALUE : nums1[cut1 - 1];
+            double L2 = (cut2 == 0) ? MIN_VALUE : nums2[cut2 - 1];
+            double R1 = (cut1 == N1) ? MAX_VALUE : nums1[cut1];
+            double R2 = (cut2 == N2) ? MAX_VALUE : nums2[cut2];
+            if (L1 > R2)
+                cutR = cut1 - 1;
+            else if (L2 > R1)
+                cutL = cut1 + 1;
+            else {// Otherwise, that's the right cut.
+                if (size % 2 == 0) {// 偶数个数的时候
+                    L1 = (L1 > L2 ? L1 : L2);
+                    R1 = (R1 < R2 ? R1 : R2);
+                    return (L1 + R1) / 2;
+                }
+
+                else {
+                    R1 = (R1 < R2 ? R1 : R2);
+                    return R1;
+                }
+            }
+        }
+        return -1;
+————————————————
+版权声明：本文为CSDN博主「immoshi」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/chen_xinjia/article/details/69258706
+ */
